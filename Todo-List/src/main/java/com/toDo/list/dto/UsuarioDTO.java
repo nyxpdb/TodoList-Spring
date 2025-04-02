9e155c77c2c8ac828a99c680d7008d01778918cf
Nyx <ana01cardoso02@gmail.com>
2025-04-02 01:17:00 -0300

    Update UsuarioDTO.java

--- Todo-List/src/main/java/com/toDo/list/dto/UsuarioDTO.java
+++ Todo-List/src/main/java/com/toDo/list/dto/UsuarioDTO.java
@@ -28,27 +28,33 @@ public class UsuarioDTO
         this.email = entity.getEmail();
     }
 
-    public Long getId() {
+    public Long getId() 
+    {
         return id;
     }
 
-    public String getNome() {
+    public String getNome() 
+    {
         return nome;
     }
 
-    public String getEmail() {
+    public String getEmail()
+    {
         return email;
     }
 
-    public void setId(Long id) {
+    public void setId(Long id) 
+    {
         this.id = id;
     }
 
-    public void setNome(String nome) {
+    public void setNome(String nome) 
+    {
         this.nome = nome;
     }
 
-    public void setEmail(String email) {
+    public void setEmail(String email) 
+    {
         this.email = email;
     }
 }
